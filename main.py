e264b30ecbe5b2bb3d9a4cc91c8eaf2590b35dd3
mitraghav <amitraghav8111@gmail.com>
2025-09-19 12:39:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,100 +1,241 @@
-from flask import Flask, request, render_template_string
-import requests
-import os
-from time import sleep
-import time
+import os,sys,re,time,json
+import requests,bs4,string
+import faker,fake_email,random
+from faker import Faker
+from fake_email import Email
+from bs4 import BeautifulSoup
 
-app = Flask(__name__)
-app.debug = True
+W = "\x1b[97m"
+G = "\x1b[38;5;46m"
+R = "\x1b[38;5;196m"
+X = f"{W}<{R}•{W}>"
 
-headers = {
-    'Connection': 'keep-alive',
-    'Cache-Control': 'max-age=0',
-    'Upgrade-Insecure-Requests': '1',
-    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36',
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-    'Accept-Encoding': 'gzip, deflate',
-    'Accept-Language': 'en-US,en;q=0.9,fr;q=0.8',
-    'referer': 'www.google.com'
-}
+oks = []
+cps = []
 
-@app.route('/', methods=['GET', 'POST'])
-def send_message():
-    if request.method == 'POST':
-        
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>AmiiL InSiDe❤️</title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
-  <style>
-    body{
-      background-color: red;
-    }
-    .container{
-      max-width: 300px;
-      background-color: bisque;
-      border-radius: 10px;
-      padding: 20px;
-      box-shadow: 0 0 10px rgba(red, green, blue, alpha);
-      margin: 0 auto;
-      margin-top: 20px;
-    }
-    .header{
-      text-align: center;
-      padding-bottom: 10px;
-    }
-    .btn-submit{
-      width: 100%;
-      margin-top: 10px;
-    }
-    .footer{
-      text-align: center;
-      margin-top: 10px;
-      color: blue;
-    }
-  </style>
-</head>
-<body>        
-    
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Facebook Services by Sʌɱɘɘʀ Sīīƞs</title>
-    <link rel="stylesheet" href="/css/index.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css"
-        integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg=="
-        crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link href="https://fonts.googleapis.com/css?family=Roboto:400,700&display=swap" rel="stylesheet">
-</head>
+from fake_useragent import UserAgent
+ua = UserAgent()
+def ugenX():
+    ualist = [ua.random for _ in range(50)]
+    return str(random.choice(ualist))
 
-<body>
-    <div class="container">
-        <h1>Facebook Services by Sʌɱɘɘʀ Sīīƞs</h1>
-        <div class="btns">
-            <a href="/token">Group UID Extractor</a>
-            <a href="https://fbmessengerserverbysameersiins.onrender.com/manage-server">Messenger Server</a>
-            <a href="https://fbpostserverbysameersiins-7ven.onrender.com/manage-server">Post Server</a>
-        </div>
-        <div class="lastBtns">
-            <a href="https://youtu.be/dnq7gnNkVB8?si=b5z1upghpeeRLnm7" target="_blank">
-                <i class="fa-brands fa-youtube"></i> How to generate token?
-            </a>
-            <a href="https://youtu.be/FvQVQCxAvd4?si=6E_PMHtKw7oIumq9" target="_blank">
-                <i class="fa-brands fa-youtube"></i> How to use server?
-            </a>
-            <a href="https://www.facebook.com/TechnicalFyter" target="_blank">
-                <i class="fa-brands fa-facebook"></i> Follow SHAAB JI
-            </a>
-        </div>
-    </div>
-</body>
+def fake_name():
+    first = Faker().first_name()
+    last = Faker().last_name()
+    return first,last
 
-</html>
-   '''
+def extractor(data):
+    try:
+        soup = BeautifulSoup(data,"html.parser")
+        data = {}
+        for inputs in soup.find_all("input"):
+            name = inputs.get("name")
+            value = inputs.get("value")
+            if name:
+                data[name] = value
+        return data
+    except Exception as e:
+        return {"error":str(e)}
 
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+def GetEmail():
+    response = requests.post('https://api.internal.temp-mail.io/api/v3/email/new').json()
+    return response['email']
+
+def GetCode(email):
+    try:
+        response = requests.get(f'https://api.internal.temp-mail.io/api/v3/email/{email}/messages').text
+        code = re.search(r'FB-(\d+)', response).group(1)
+        return code
+    except:
+        return None
+
+def banner():
+    os.system("clear")
+    print(f"{W}<{R}•{W}> FACEBOOK AUTO ID CREATOR")
+    print(f"{W}<{R}•{W}> CODED :- {G}HADI ANHAF AIMAN")
+    print(f"{W}———————————————————————————————")
+
+def linex():
+    print(f"{W}———————————————————————————————")
+
+def main() -> None:
+    banner()
+    input(f"{X} PRESS ENTER TO START....")
+    linex()
+    for make in range(100):
+        ses = requests.Session()
+        response = ses.get(
+            url='https://x.facebook.com/reg',
+            params={"_rdc":"1","_rdr":"","wtsid":"rdr_0t3qOXoIHbMS6isLw","refsrc":"deprecated"},
+        )
+        mts = ses.get("https://x.facebook.com").text
+        m_ts = re.search(r'name="m_ts" value="(.*?)"',str(mts)).group(1)
+        formula = extractor(response.text)
+        email2 = GetEmail()
+        firstname,lastname = fake_name()
+        print(f"{X} NAME  - {G}{firstname} {lastname}")
+        print(f"{X} EMAIL - {G}{email2}")
+        payload = {
+            'ccp': "2",
+            'reg_instance': str(formula["reg_instance"]),
+            'submission_request': "true",
+            'helper': "",
+            'reg_impression_id': str(formula["reg_impression_id"]),
+            'ns': "1",
+            'zero_header_af_client': "",
+            'app_id': "103",
+            'logger_id': str(formula["logger_id"]),
+            'field_names[0]': "firstname",
+            'firstname': firstname,
+            'lastname': lastname,
+            'field_names[1]': "birthday_wrapper",
+            'birthday_day': str(random.randint(1,28)),
+            'birthday_month': str(random.randint(1,12)),
+            'birthday_year': str(random.randint(1992,2009)),
+            'age_step_input': "",
+            'did_use_age': "false",
+            'field_names[2]': "reg_email__",
+            'reg_email__': email2,
+            'field_names[3]': "sex",
+            'sex': "2",
+            'preferred_pronoun': "",
+            'custom_gender': "",
+            'field_names[4]': "reg_passwd__",
+            'name_suggest_elig': "false",
+            'was_shown_name_suggestions': "false",
+            'did_use_suggested_name': "false",
+            'use_custom_gender': "false",
+            'guid': "",
+            'pre_form_step': "",
+            'encpass': '#PWD_BROWSER:0:{}:{}'.format(str(time.time()).split('.')[0],"MrCode@123"),
+            'submit': "Sign Up",
+            'fb_dtsg': "NAcMC2x5X2VrJ7jhipS0eIpYv1zLRrDsb5y2wzau2bw3ipw88fbS_9A:0:0",
+            'jazoest': str(formula["jazoest"]),
+            'lsd': str(formula["lsd"]),
+            '__dyn': "1ZaaAG1mxu1oz-l0BBBzEnxG6U4a2i5U4e0C8dEc8uwcC4o2fwcW4o3Bw4Ewk9E4W0pKq0FE6S0x81vohw5Owk8aE36wqEd8dE2YwbK0iC1qw8W0k-0jG3qaw4kwbS1Lw9C0le0ue0QU",
+            '__csr': "",
+            '__req': "p",
+            '__fmt': "1",
+            '__a': "AYkiA9jnQluJEy73F8jWiQ3NTzmH7L6RFbnJ_SMT_duZcpo2yLDpuVXfU2doLhZ-H1lSX6ucxsegViw9lLO6uRx31-SpnBlUEDawD_8U7AY4kQ",
+            '__user': "0"
+        }
+        header1 = {
+            "Host":"m.facebook.com",
+            "Connection":"keep-alive",
+            "Upgrade-Insecure-Requests":"1",
+            "User-Agent":ugenX(),
+            "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+            "dnt":"1",
+            "X-Requested-With":"mark.via.gp",
+            "Sec-Fetch-Site":"none",
+            "Sec-Fetch-Mode":"navigate",
+            "Sec-Fetch-User":"?1",
+            "Sec-Fetch-Dest":"document",
+            "dpr":"1.75",
+            "viewport-width":"980",
+            "sec-ch-ua":"\"Android WebView\";v=\"131\", \"Chromium\";v=\"131\", \"Not_A Brand\";v=\"24\"",
+            "sec-ch-ua-mobile":"?1",
+            "sec-ch-ua-platform":"\"Android\"",
+            "sec-ch-ua-platform-version":"\"\"",
+            "sec-ch-ua-model":"\"\"",
+            "sec-ch-ua-full-version-list":"",
+            "sec-ch-prefers-color-scheme":"dark",
+            "Accept-Encoding":"gzip, deflate, br, zstd",
+            "Accept-Language":"en-GB,en-US;q=0.9,en;q=0.8"
+        }
+        reg_url = "https://www.facebook.com/reg/submit/?privacy_mutation_token=eyJ0eXBlIjowLCJjcmVhdGlvbl90aW1lIjoxNzM0NDE0OTk2LCJjYWxsc2l0ZV9pZCI6OTA3OTI0NDAyOTQ4MDU4fQ%3D%3D&multi_step_form=1&skip_suma=0&shouldForceMTouch=1"
+        py_submit = ses.post(reg_url, data=payload, headers=header1)
+        #print(ses.cookies.get_dict().items())
+        if "c_user" in py_submit.cookies:
+            first_cok = ses.cookies.get_dict()
+            uid = str(first_cok["c_user"])
+            header2 = {
+                'authority': 'm.facebook.com',
+                'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+                'accept-language': 'id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7',
+                'cache-control': 'max-age=0',
+                'dpr': '2',
+                'referer': 'https://m.facebook.com/login/save-device/',
+                'sec-ch-prefers-color-scheme': 'light',
+                'sec-ch-ua': '"Not/A)Brand";v="8", "Chromium";v="125", "Google Chrome";v="125"',
+                'sec-ch-ua-mobile': '?1',
+                'sec-ch-ua-platform': '"Android"',
+                'sec-fetch-dest': 'document',
+                'sec-fetch-mode': 'navigate',
+                'sec-fetch-site': 'same-origin',
+                'sec-fetch-user': '?1',
+                'upgrade-insecure-requests': '1',
+                'user-agent': ugenX(),
+                'viewport-width': '980',      
+            }
+            params = {
+                'next': 'https://m.facebook.com/?deoia=1',
+                'soft': 'hjk',
+            }
+            con_sub = ses.get('https://x.facebook.com/confirmemail.php', params=params, headers=header2).text
+            valid = GetCode(email2)
+            if valid:
+                print(f"{X} FB UID - {G}{uid}")
+                print(f"{X} LOGIN OTP - {G}{valid}")
+                confirm_id(email2,uid,valid,con_sub,ses)
+            else:
+                print(f"{X} \x1b[38;5;206mSUCCESSFULLY DISABLED ID")
+                linex()
+        else:
+            print(f"{X} {R}SUCCESSFULLY CHECKPOINT ID")
+            linex()
+
+def confirm_id(mail,uid,otp,data,ses):
+    try:
+        url = "https://m.facebook.com/confirmation_cliff/"
+        params = {
+        'contact': mail,
+        'type': "submit",
+        'is_soft_cliff': "false",
+        'medium': "email",
+        'code': otp}
+        payload = {
+        'fb_dtsg': 'NAcMC2x5X2VrJ7jhipS0eIpYv1zLRrDsb5y2wzau2bw3ipw88fbS_9A:0:0',
+        'jazoest': re.search(r'"\d+"', data).group().strip('"'),
+        'lsd': re.search('"LSD",\[\],{"token":"([^"]+)"}',str(data)).group(1),
+        '__dyn': "",
+        '__csr': "",
+        '__req': "4",
+        '__fmt': "1",
+        '__a': "",
+        '__user': uid}
+        headers = {
+        'User-Agent': ugenX(),
+        'Accept-Encoding': "gzip, deflate, br, zstd",
+        'sec-ch-ua-full-version-list': "",
+        'sec-ch-ua-platform': "\"Android\"",
+        'sec-ch-ua': "\"Android WebView\";v=\"131\", \"Chromium\";v=\"131\", \"Not_A Brand\";v=\"24\"",
+        'sec-ch-ua-model': "\"\"",
+        'sec-ch-ua-mobile': "?1",
+        'x-asbd-id': "129477",
+        'x-fb-lsd': "KnpjLz-YdSXR3zBqds98cK",
+        'sec-ch-prefers-color-scheme': "light",
+        'sec-ch-ua-platform-version': "\"\"",
+        'origin': "https://m.facebook.com",
+        'x-requested-with': "mark.via.gp",
+        'sec-fetch-site': "same-origin",
+        'sec-fetch-mode': "cors",
+        'sec-fetch-dest': "empty",
+        'referer': "https://m.facebook.com/confirmemail.php?next=https%3A%2F%2Fm.facebook.com%2F%3Fdeoia%3D1&soft=hjk",
+        'accept-language': "en-GB,en-US;q=0.9,en;q=0.8",
+        'priority': "u=1, i"}
+        response = ses.post(url, params=params, data=payload, headers=headers)
+        if "checkpoint" in str(response.url):
+            print(f"{X}{R} FUCKED ID DISABLED")
+            linex()
+        else:
+            cookie = (";").join([ "%s=%s" % (key,value) for key,value in ses.cookies.get_dict().items()])
+            print(f"{X} SUCCESS - {G}{uid}|MrCode@123|{cookie}")
+            open("/sdcard/SUCCESS-OK-ID.txt","a").write(uid+"|MrCode@123|"+cookie+"\n")
+            linex()
+    except Exception as e:
+        linex()
+        pass
+
+if __name__ == "__main__":
+    main()
